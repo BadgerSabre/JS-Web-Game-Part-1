12911a26f5c09692a1621669a8759a10da9e5bfb
Brandon Dega <brandondega@gmail.com>
2022-03-23 18:47:04 -0500

    added items

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-// function newImage(imageName, imageSrc, left, bottom) {
-//     let imageName = document.createElement('img')
-//     imageName.src = imageSrc
-//     imageName.style.position = 'fixed'
-//     imageName.style.left = `${left}`
-//     imageName.style.bottom = `${bottom}`
-//     document.body.append(imageName)
-// }
-
-// newImage(assets/green-character.gif, 100, 100)
-
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
-
-// newImage(pineTree, assets/pine-tree.png, 450, 200)
-
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+function newImage(imageSrc, left, bottom) {
+    let item = document.createElement('img')
+    item.src = imageSrc
+    item.style.position = 'fixed'
+    item.style.left = `${left}px`
+    item.style.bottom = `${bottom}px`
+    document.body.append(item)
+}
+// static items //
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
