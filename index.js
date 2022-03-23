b32d810c4651996a20084e174b99105b84fd037d
Brandon Dega <brandondega@gmail.com>
2022-03-23 17:03:40 -0500

    tweaked function

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-function newImage(image, imageName, left, bottom) {
-    let image = document.createElement('img')
-    image.src = `assets/${imageName}`
-    image.style.position = 'fixed'
-    image.style.left = `${left}px`
-    image.style.bottom = `${bottom}px`
-    document.body.append(image)
-}
+// function newImage(imageName, imageSrc, left, bottom) {
+//     let imageName = document.createElement('img')
+//     imageName.src = imageSrc
+//     imageName.style.position = 'fixed'
+//     imageName.style.left = `${left}`
+//     imageName.style.bottom = `${bottom}`
+//     document.body.append(imageName)
+// }
 
-newImage(greenCharacter, green-character.gif, 100, 100)
+// newImage(assets/green-character.gif, 100, 100)
 
-// let greenCharacter = document.createElement('img')
-// greenCharacter.src = 'assets/green-character.gif'
-// greenCharacter.style.position = 'fixed'
-// greenCharacter.style.left = '100px'
-// greenCharacter.style.bottom = '100px'
-// document.body.append(greenCharacter)
+let greenCharacter = document.createElement('img')
+greenCharacter.src = 'assets/green-character.gif'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '100px'
+greenCharacter.style.bottom = '100px'
+document.body.append(greenCharacter)
+
+// newImage(pineTree, assets/pine-tree.png, 450, 200)
 
 let pineTree = document.createElement('img')
 pineTree.src = 'assets/pine-tree.png'
