8f4be7990d4cf3858a606784f7bda74e27255070
Brandon Dega <brandondega@gmail.com>
2022-03-24 09:34:17 -0500

    added sky, grass, equipable items

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 // functions //
-function newImage(imageSrc, left, bottom) {
+function newImage(imageSrc, left, bottom, equipableItem = false) {
     let item = document.createElement('img')
     item.src = imageSrc
     item.style.position = 'fixed'
     item.style.left = `${left}px`
     item.style.bottom = `${bottom}px`
     document.body.append(item)
+    if(equipableItem){
+        item.addEventListener('dblclick', function() {
+            item.remove()
+        })
+    }
 }
 
-function newItem(imageSrc, left, bottom) {
-    newImage(imageSrc, left, bottom)
-    item.addEventListener('click', function() {
-        item.remove()
-    })
-}
-
-// static images //
+// static items //
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
@@ -24,6 +22,20 @@ newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
 // equipable items //
-newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
+newImage('assets/sword.png', 500, 405, true)
+newImage('assets/sheild.png', 165, 185, true)
+newImage('assets/staff.png', 600, 100, true)
+
+// sky //
+for(let i = 0; i < 76; i++){
+    let skyTile = document.createElement('img')
+    skyTile.src = './assets/sky.png'
+    document.body.append(skyTile)
+}
+
+// grass //
+for(let i = 0; i < 114; i++){
+    let grassTile = document.createElement('img')
+    grassTile.src = './assets/grass.png'
+    document.body.append(grassTile)
+}
