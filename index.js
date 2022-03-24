f2dddaf336aedf24759b0105e604ef6aba0afe1d
Brandon Dega <brandondega@gmail.com>
2022-03-24 18:20:13 -0500

    simiplified functions

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // functions //
-function newImage(imageSrc, left, bottom, equipableItem = false) {
+function newImage(imageSrc, left, bottom) {
     let item = document.createElement('img')
     item.src = imageSrc
     item.style.position = 'fixed'
     item.style.left = `${left}px`
     item.style.bottom = `${bottom}px`
     document.body.append(item)
-    if(equipableItem){
-        item.addEventListener('dblclick', function() {
-            item.remove()
-        })
-    }
+    return item
+}
+
+
+function newItem(imageSrc, left, bottom) {
+    let item = newImage(imageSrc, left, bottom)
+
+    item.addEventListener('dblclick', () => {
+        item.remove()
+    })
 }
 
 // static items //
@@ -22,9 +27,9 @@ newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
 // equipable items //
-newImage('assets/sword.png', 500, 405, true)
-newImage('assets/sheild.png', 165, 185, true)
-newImage('assets/staff.png', 600, 100, true)
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
 
 // sky //
 for(let i = 0; i < 76; i++){
