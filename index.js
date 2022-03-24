307521cce3c93a9fe323f59beae74f78e794795b
Brandon Dega <brandondega@gmail.com>
2022-03-23 19:01:51 -0500

    added newItem function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// functions //
 function newImage(imageSrc, left, bottom) {
     let item = document.createElement('img')
     item.src = imageSrc
@@ -6,10 +7,23 @@ function newImage(imageSrc, left, bottom) {
     item.style.bottom = `${bottom}px`
     document.body.append(item)
 }
-// static items //
+
+function newItem(imageSrc, left, bottom) {
+    newImage(imageSrc, left, bottom)
+    item.addEventListener('click', function() {
+        item.remove()
+    })
+}
+
+// static images //
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/pine-tree.png', 450, 200)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
+
+// equipable items //
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
